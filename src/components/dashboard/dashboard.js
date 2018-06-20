35ee0705ce1c3fd2e1f96fa2c64fe507bdee0450
Kris Sakarias <ksakarias@gmail.com>
2018-06-20 14:50:28 -0700

    testing dashboard render

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -4,9 +4,17 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import PictureForm from './../picture-form/picture-form';
 import * as pictureActions from '../../actions/picture-actions';
+import * as profileActions from '../../actions/profile-action';
 
 
 class Dashboard extends React.Component {
+  componentDidMount() {
+    if (this.props.loggedIn) {
+      this.props.pFetchProfile()
+        .catch(console.error);
+    }
+  }
+
   render() {
     const {
       pictures, pictureCreate, pictureUpdate, pictureDelete,
@@ -23,20 +31,25 @@ class Dashboard extends React.Component {
   }
 }
 Dashboard.propTypes = {
+  pFetchProfile: PropTypes.func,
+  profileCreate: PropTypes.func,
   picturesFetch: PropTypes.func,
   pictureCreate: PropTypes.func,
   pictureUpdate: PropTypes.func,
   pictureDelete: PropTypes.func,
   pictures: PropTypes.array,
+  profile: PropTypes.object,
 };
 
 const mapStateToProps = (state) => {
   return {
-    parcels: state.pictures,
+    pictures: state.pictures,
+    profile: state.profile,
   };
 };
 
 const mapDispatchToProps = dispatch => ({
+  pFetchProfile: () => dispatch(profileActions.fetchRequest()),
   picturesFetch: () => dispatch(pictureActions.picturesFetchRequest()),
   pictureCreate: picture => dispatch(pictureActions.pictureCreateRequest(picture)),
   pictureUpdate: picture => dispatch(pictureActions.pictureUpdateRequest(picture)),
