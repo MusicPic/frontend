07f933a24c3b215c72b72807f8cfacbbddce04a1
Kris <kris71990@users.noreply.github.com>
2018-06-21 10:07:17 -0700

    Update routes.js
    adding process.env

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,5 @@ export const PROFILE_ROUTE = '/profile';
 export const PICTURE_ROUTE = '/picture';
 export const PLAYLIST_ROUTE = '/playlist';
 export const LOGIN_ROUTE = '/login';
-export const SPOTIFY_ROUTE = `https://accounts.spotify.com/authorize/?response_type=code&client_id=${CLIENT_ID}&scope=user-read-private%20user-read-email%20user-top-read%20user-library-read%20user-read-birthdate%20user-follow-read&redirect_uri=${API_URL}/login`;
+export const SPOTIFY_ROUTE = `https://accounts.spotify.com/authorize/?response_type=code&client_id=${process.env.CLIENT_ID}&scope=user-read-private%20user-read-email%20user-top-read%20user-library-read%20user-read-birthdate%20user-follow-read&redirect_uri=${process.env.API_URL}/login`;
 
